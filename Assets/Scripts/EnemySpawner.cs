bf36d382238d6e1043d20cecd1e5f45e18608986
divyeanand <2006070@kiit.ac.in>
2023-04-25 13:01:43 +0530

    Initial Commit

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public List<Transform> spwanPoints;
+    public GameObject enemyPrefab, enemies;
+
+    public float enemyBurstCount=3, spawnTime = 1;
+
+    Transform location;
+    float updateTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+            spwanPoints.Add(child);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > updateTime){
+            updateTime = Time.time + spawnTime;
+            SpwanEnemy();
+        }
+        
+    }
+
+    public void SpwanEnemy(){
+        if(enemies.transform.childCount < enemyBurstCount){
+        location = spwanPoints[Random.Range(0, transform.childCount)];
+        var enemyInstance = Instantiate(enemyPrefab, location);
+        enemyInstance.transform.SetParent(enemies.transform);
+        enemyInstance.transform.LookAt(Vector3.zero);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static float playerHealth, currentScore;
+    public Image healthImg;
+    public TMP_Text scoreText, highScoreText;
+    public GameObject gameON, gameOFF, enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        highScoreText.text = "High Score = " + PlayerPrefs.GetFloat("HighScore").ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerHealth <= 0)
+           GameOver();
+        healthImg.fillAmount = playerHealth;
+        scoreText.text = currentScore.ToString();        
+    }
+
+    public void GameStart(){
+        gameON.SetActive(true);
+        gameOFF.SetActive(false);
+        playerHealth = 1;
+        currentScore = 0;
+        scoreText.text = "0";
+    }
+
+    public void GameOver(){
+        if(currentScore>PlayerPrefs.GetFloat("HighScore")){
+        PlayerPrefs.SetFloat("HighScore", currentScore);
+        highScoreText.text = "High Score = " + PlayerPrefs.GetFloat("HighScore").ToString();
+        }
+        foreach (Transform child in enemies.transform)
+            child.gameObject.GetComponent<EnemyController>().Death();
+        gameON.SetActive(false);
+        gameOFF.SetActive(true);
+    }
+}
